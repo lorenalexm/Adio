978f97fbc4409dc3f8fee115724cfe5ba77c673b
Alex Loren <lorenalexm@gmail.com>
2022-11-17 14:22:21 -0800

    Created new view for NowPlaying

---
+++ Adio/Views/NowPlayingView.swift
@@ -0,0 +1,20 @@
+//
+//  NowPlayingView.swift
+//  Adio
+//
+//  Created by Alex Loren on 11/17/22.
+//
+
+import SwiftUI
+
+struct NowPlayingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NowPlayingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NowPlayingView()
+    }
+}
