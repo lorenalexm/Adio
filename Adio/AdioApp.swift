2797d5b4e972befa515031b8ec2a4ce7e5eb315a
Alex Loren <lorenalexm@gmail.com>
2022-11-16 17:44:50 -0800

    Initial Commit

---
+++ Adio/AdioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdioApp.swift
+//  Adio
+//
+//  Created by Alex Loren on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AdioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
