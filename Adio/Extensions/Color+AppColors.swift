7c8ac9a4ec25978dcff3c4f4b47902ceefd799c5
Alex Loren <lorenalexm@gmail.com>
2022-11-17 14:56:12 -0800

    Added custom colors extension

---
+++ Adio/Extensions/Color+AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  Color+AppColors.swift
+//  Adio
+//
+//  Created by Alex Loren on 11/17/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundLight = Color(red: 0.357, green: 0.349, blue: 0.557)
+    static let backgroundDark = Color(red: 0.306, green: 0.306, blue: 0.478)
+    static let text = Color(red: 0.988, green: 0.996, blue: 0.996)
+}
