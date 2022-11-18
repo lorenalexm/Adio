ba9acba6a0a006660708f9a8cb2d5ea25d08b05b
Alex Loren <lorenalexm@gmail.com>
2022-11-18 14:05:31 -0800

    Embedded views within a TabView, styled tab bar

--- Adio/AdioApp.swift
+++ Adio/AdioApp.swift
@@ -11,7 +11,21 @@ import SwiftUI
 struct AdioApp: App {
     var body: some Scene {
         WindowGroup {
-            Text("Coming soon.")
+            TabView {
+                NowPlayingView()
+                    .tabItem {
+                        Label("Now Playing", systemImage: "play.fill")
+                    }
+            }
+            .onAppear {
+                let appearance = UITabBarAppearance()
+                appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+                //appearance.backgroundColor = UIColor(Color.backgroundLight.opacity(0.2))
+                
+                UITabBar.appearance().standardAppearance = appearance
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
+            .tint(Color.text)
         }
     }
 }
