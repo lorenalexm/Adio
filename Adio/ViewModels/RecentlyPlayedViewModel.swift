2e25839e96c66fb9a8f00421a3e196470c6fd7e0
Alex Loren <lorenalexm@gmail.com>
2022-11-19 17:02:17 -0800

    Created view model and tests for RecentlyPlayed

--- Adio/ViewModels/RecentlyPlayedViewModel.swift
+++ Adio/ViewModels/RecentlyPlayedViewModel.swift
@@ -9,5 +9,30 @@ import SwiftUI
 
 @MainActor class RecentlyPlayedViewModel: ObservableObject {
     // MARK: - Properties.
-    @Published var recentlyPlayed = RecentlyPlayed()
+    @Published var recentlyPlayed = [SongContainer]()
+    private var urlSession = URLSession.shared
+    
+    // MARK: - Functions.
+    
+    /// Fetches the Radio from the remote `URL` and attempts to parse the `Song` history from the data.
+    /// - Parameter url: The `URL` of the remote AzuraCast server.
+    /// - Returns: An array of `SongContainer` objects, guarenteed to have 1 or more elements.
+    /// - Throws: `URLError.badServerResponse` if status code does not equal 200.
+    /// - Throws: A `fatalError` if the song history does not contain at least one element.
+    public func fetchRecentlyPlayed(from url: String) async throws -> [SongContainer] {
+        let (data, response) = try await urlSession.data(from: URL(string: url)!)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+
+        let fetched = try JSONDecoder().decode(Radio.self, from: data)
+        guard fetched.count > 0,
+              let recentlyPlayed: [SongContainer] = fetched[0].songHistory,
+              recentlyPlayed.count > 0 else {
+            fatalError("Failed to decode recently played")
+        }
+        
+        self.recentlyPlayed = recentlyPlayed
+        return recentlyPlayed
+    }
 }
--- AdioTests/RecentlyPlayedViewModelTests.swift
+++ AdioTests/RecentlyPlayedViewModelTests.swift
@@ -5,4 +5,18 @@
 //  Created by Alex Loren on 11/19/22.
 //
 
-import Foundation
+import XCTest
+@testable import Adio
+
+@MainActor final class RecentlyPlayedViewModelTests: XCTestCase {
+    // MARK: - Tests.
+    /// Fetches the now playing information from the AzuraCast demo server. Attempts to parse the song history from the data.
+    /// This test may fail if near the bottom of the hour, as the server metrics reset hourly.
+    func testFetchingRecentlyPlayed() async {
+        let viewModel = RecentlyPlayedViewModel()
+
+        XCTAssertEqual(viewModel.recentlyPlayed.count, 0)
+        _ = try? await viewModel.fetchRecentlyPlayed(from: "https://demo.azuracast.com/api/nowplaying")
+        XCTAssertEqual(viewModel.recentlyPlayed.count, 5)
+    }
+}
